22f90fb3aa671f4ced310901681b09275c0008a5
Werbison Oliveira <werbison090@gmail.com>
2025-08-22 14:35:05 -0300

    Animações da página finalizada

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,12 +2,39 @@
 //seleções
 const menu = document.querySelector(".bi");
 const options = document.querySelector("#menu");
+const textBannerH1 = document.querySelector(".text-banner h1");
+const textBannerP = document.querySelector(".text-banner p");
+const cards = document.querySelectorAll(".card");
 
 //funções
-const checkMenu = () => {
-    options.classList.toggle("hide");
+
+function animation() {
+
+    const observer = new IntersectionObserver((entries, observer) => {
+
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add("animation-card");
+            }
+
+            observer.unobserve(entry.target);
+
+        })
+
+    }, {}); 
+
+    cards.forEach((card) => {
+        observer.observe(card);
+    });
 }
+
 //eventos
 menu.addEventListener("click", () => {
-    checkMenu();
+    options.classList.toggle("hide");
+});
+
+window.addEventListener("load", () => {
+    textBannerH1.classList.add("animation");
+    textBannerP.classList.add("animation");
+    animation();
 });

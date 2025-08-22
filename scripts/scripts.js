4f7ad1305e0b61f193f67409dc480e283ad825c9
Werbison Oliveira <werbison090@gmail.com>
2025-08-21 22:03:42 -0300

    Responsividade finalizada

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,14 @@
 "use strict";
 //seleções
+const menu = document.querySelector(".bi");
+const options = document.querySelector("#menu");
+console.log(options)
 
 //funções
-
+const checkMenu = () => {
+    options.classList.toggle("hide");
+}
 //eventos
+menu.addEventListener("click", () => {
+    checkMenu();
+})
